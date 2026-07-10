netty/netty-incubator-codec-ohttp

commit 3dca98770ea5b6521fa40a8614fcc4883079c9fa

OHTTP: Add support for XWING (#134)
Motivation:

To support PQC we need to support some KEM like XWING

Modifications:

Support XWING in our native implementation using BoringSSL

Result:

Be able to use XWING for OHTTP

codec-ohttp-hpke/src/main/java/io/netty/incubator/codec/hpke/KEM.java
@@ -23,7 +23,9 @@ public enum KEM {
     P384_SHA348((short) 17, 97, 97),
     P521_SHA512((short) 18, 133, 133),
     X25519_SHA256((short) 32, 32, 32),
-    X448_SHA512((short) 33, 56, 56);
+    X448_SHA512((short) 33, 56, 56),
+    // See https://datatracker.ietf.org/doc/draft-connolly-cfrg-xwing-kem/
+    XWING((short) 0x647a, 1120, 1216);
 
     public static KEM forId(short id) {
         for (KEM val : values()) {

codec-ohttp-hpke-classes-boringssl/src/main/java/io/netty/incubator/codec/hpke/boringssl/BoringSSL.java
@@ -61,6 +61,8 @@ final class BoringSSL {
 
     static final long EVP_hpke_x25519_hkdf_sha256 =
             BoringSSLNativeStaticallyReferencedJniMethods.EVP_hpke_x25519_hkdf_sha256();
+    static final long EVP_hpke_xwing =
+            BoringSSLNativeStaticallyReferencedJniMethods.EVP_hpke_xwing();
     static final long EVP_hpke_hkdf_sha256 =
             BoringSSLNativeStaticallyReferencedJniMethods.EVP_hpke_hkdf_sha256();
     static final long EVP_hpke_aes_128_gcm =

codec-ohttp-hpke-classes-boringssl/src/main/java/io/netty/incubator/codec/hpke/boringssl/BoringSSLNativeStaticallyReferencedJniMethods.java
@@ -17,6 +17,7 @@ package io.netty.incubator.codec.hpke.boringssl;
 
 final class BoringSSLNativeStaticallyReferencedJniMethods {
     static native long EVP_hpke_x25519_hkdf_sha256();
+    static native long EVP_hpke_xwing();
     static native long EVP_hpke_hkdf_sha256();
     static native long EVP_hpke_aes_128_gcm();
     static native long EVP_hpke_aes_256_gcm();

codec-ohttp-hpke-classes-boringssl/src/main/java/io/netty/incubator/codec/hpke/boringssl/BoringSSLOHttpCryptoProvider.java
@@ -89,10 +89,14 @@ public final class BoringSSLOHttpCryptoProvider implements OHttpCryptoProvider {
     }
 
     private static long boringSSLKEM(KEM kem) {
-        if (kem != KEM.X25519_SHA256) {
-            throw new IllegalArgumentException("KEM not supported: " + kem);
+        switch (kem) {
+            case XWING:
+                return BoringSSL.EVP_hpke_xwing;
+            case X25519_SHA256:
+                return BoringSSL.EVP_hpke_x25519_hkdf_sha256;
+            default:
+                throw new IllegalArgumentException("KEM not supported: " + kem);
         }
-        return BoringSSL.EVP_hpke_x25519_hkdf_sha256;
     }
 
     private static long boringSSLHPKEAEAD(AEAD aead) {
@@ -264,7 +268,13 @@ public final class BoringSSLOHttpCryptoProvider implements OHttpCryptoProvider {
 
     @Override
     public boolean isSupported(KEM kem) {
-        return kem == KEM.X25519_SHA256;
+        switch (kem) {
+            case X25519_SHA256:
+            case XWING:
+                return true;
+            default:
+                return false;
+        }
     }
 
     @Override

codec-ohttp-hpke-native-boringssl/src/main/c/netty_incubator_codec_ohttp_hpke_boringssl.c
@@ -53,6 +53,10 @@ static jlong netty_incubator_codec_ohttp_hpke_boringssl_EVP_hpke_x25519_hkdf_sha
     return (jlong) EVP_hpke_x25519_hkdf_sha256();
 }
 
+static jlong netty_incubator_codec_ohttp_hpke_boringssl_EVP_hpke_xwing(JNIEnv* env, jclass clazz) {
+    return (jlong) EVP_hpke_xwing();
+}
+
 static jlong netty_incubator_codec_ohttp_hpke_boringssl_EVP_hpke_hkdf_sha256(JNIEnv* env, jclass clazz) {
     return (jlong) EVP_hpke_hkdf_sha256();
 }
@@ -497,6 +501,7 @@ cleanup:
 // JNI Method Registration Table Begin
 static const JNINativeMethod statically_referenced_fixed_method_table[] = {
   { "EVP_hpke_x25519_hkdf_sha256", "()J", (void *) netty_incubator_codec_ohttp_hpke_boringssl_EVP_hpke_x25519_hkdf_sha256 },
+  { "EVP_hpke_xwing", "()J", (void *) netty_incubator_codec_ohttp_hpke_boringssl_EVP_hpke_xwing },
   { "EVP_hpke_hkdf_sha256", "()J", (void *) netty_incubator_codec_ohttp_hpke_boringssl_EVP_hpke_hkdf_sha256 },
   { "EVP_hpke_aes_128_gcm", "()J", (void *) netty_incubator_codec_ohttp_hpke_boringssl_EVP_hpke_aes_128_gcm },
   { "EVP_hpke_aes_256_gcm", "()J", (void *) netty_incubator_codec_ohttp_hpke_boringssl_EVP_hpke_aes_256_gcm },

commit 4f1ae542b623a54c15d58befcf385dcb6c927c88

Fix XWING support
Motivation:

3dca98770ea5b6521fa40a8614fcc4883079c9fa introduced support for XWING when using BoringSSL but did contain a bug.

Modifications:

- Fix XWING implementation
- Add testcases

Result:

XWING works when using BoringSSL

codec-ohttp-hpke/src/main/java/io/netty/incubator/codec/hpke/KEM.java
@@ -25,7 +25,7 @@ public enum KEM {
     X25519_SHA256((short) 32, 32, 32),
     X448_SHA512((short) 33, 56, 56),
     // See https://datatracker.ietf.org/doc/draft-connolly-cfrg-xwing-kem/
-    XWING((short) 0x647a, 1120, 1216);
+    XWING((short) 0x647a, 1120, 32);
 
     public static KEM forId(short id) {
         for (KEM val : values()) {

codec-ohttp/src/main/java/io/netty/incubator/codec/ohttp/OHttpKey.java
@@ -188,7 +188,8 @@ public abstract class OHttpKey {
 
             byte[] encoded = keyPair.privateParameters().encoded();
             if (encoded != null && encoded.length != kem.npk()) {
-                throw new CryptoException("Invalid public key, pkEncoded.length does not match Npk from KEM");
+                throw new CryptoException("Invalid public key, pkEncoded.length(" + encoded.length +
+                        ") does not match Npk(" + kem.npk() + ") from KEM: " + kem);
             }
             this.keyPair = keyPair;
         }

codec-ohttp/src/test/java/io/netty/incubator/codec/ohttp/OHttpCodecsTest.java
@@ -55,6 +55,7 @@ import io.netty.incubator.codec.hpke.boringssl.BoringSSLOHttpCryptoProvider;
 import io.netty.incubator.codec.hpke.bouncycastle.BouncyCastleOHttpCryptoProvider;
 import io.netty.util.ReferenceCountUtil;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -83,26 +84,37 @@ public class OHttpCodecsTest {
             List<Arguments> arguments = new ArrayList<>();
             for (int i = 0; i < 2; i++) {
                 boolean trailers = i == 0;
-                arguments.add(Arguments.of(OHttpVersionDraft.INSTANCE, BouncyCastleOHttpCryptoProvider.INSTANCE,
-                        BouncyCastleOHttpCryptoProvider.INSTANCE, trailers));
-                arguments.add(Arguments.of(OHttpVersionChunkDraft.INSTANCE, BouncyCastleOHttpCryptoProvider.INSTANCE,
-                        BouncyCastleOHttpCryptoProvider.INSTANCE, trailers));
+                for (KEM kem: new KEM[] { KEM.X25519_SHA256, KEM.XWING }) {
+                    arguments.add(Arguments.of(
+                            OHttpVersionDraft.INSTANCE, BouncyCastleOHttpCryptoProvider.INSTANCE,
+                            BouncyCastleOHttpCryptoProvider.INSTANCE, kem, trailers));
+                    arguments.add(Arguments.of(
+                            OHttpVersionChunkDraft.INSTANCE, BouncyCastleOHttpCryptoProvider.INSTANCE,
+                            BouncyCastleOHttpCryptoProvider.INSTANCE, kem, trailers));
 
-                if (BoringSSLHPKE.isAvailable()) {
-                    arguments.add(Arguments.of(OHttpVersionDraft.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
-                            BoringSSLOHttpCryptoProvider.INSTANCE, trailers));
-                    arguments.add(Arguments.of(OHttpVersionChunkDraft.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
-                            BoringSSLOHttpCryptoProvider.INSTANCE, trailers));
+                    if (BoringSSLHPKE.isAvailable()) {
+                        arguments.add(Arguments.of(
+                                OHttpVersionDraft.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
+                                BoringSSLOHttpCryptoProvider.INSTANCE, kem, trailers));
+                        arguments.add(Arguments.of(
+                                OHttpVersionChunkDraft.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
+                                BoringSSLOHttpCryptoProvider.INSTANCE, kem, trailers));
 
-                    arguments.add(Arguments.of(OHttpVersionDraft.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
-                            BouncyCastleOHttpCryptoProvider.INSTANCE, trailers));
-                    arguments.add(Arguments.of(OHttpVersionChunkDraft.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
-                            BouncyCastleOHttpCryptoProvider.INSTANCE, trailers));
+                        arguments.add(Arguments.of(
+                                OHttpVersionDraft.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
+                                BouncyCastleOHttpCryptoProvider.INSTANCE, kem, trailers));
+                        arguments.add(Arguments.of(
+                                OHttpVersionChunkDraft.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
+                                BouncyCastleOHttpCryptoProvider.INSTANCE, kem, trailers));
 
-                    arguments.add(Arguments.of(OHttpVersionDraft.INSTANCE, BouncyCastleOHttpCryptoProvider.INSTANCE,
-                            BoringSSLOHttpCryptoProvider.INSTANCE, trailers));
-                    arguments.add(Arguments.of(OHttpVersionChunkDraft.INSTANCE,
-                            BouncyCastleOHttpCryptoProvider.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE, trailers));
+                        arguments.add(Arguments.of(
+                                OHttpVersionDraft.INSTANCE, BouncyCastleOHttpCryptoProvider.INSTANCE,
+                                BoringSSLOHttpCryptoProvider.INSTANCE, kem, trailers));
+                        arguments.add(Arguments.of(
+                                OHttpVersionChunkDraft.INSTANCE,
+                                BouncyCastleOHttpCryptoProvider.INSTANCE, BoringSSLOHttpCryptoProvider.INSTANCE,
+                                kem, trailers));
+                    }
                 }
             }
             return arguments.stream();
@@ -131,35 +143,36 @@ public class OHttpCodecsTest {
     }
 
     public static ChannelPair createChannelPair(OHttpVersion version, OHttpCryptoProvider clientProvider,
-                                                OHttpCryptoProvider serverProvider) throws Exception {
-        return createChannelPair(version, clientProvider, serverProvider,
+                                                OHttpCryptoProvider serverProvider, KEM kem) throws Exception {
+        return createChannelPair(version, clientProvider, serverProvider, kem,
                 new OHttpClientCodecBuilder(), new OHttpServerCodecBuilder());
     }
 
     public static ChannelPair createChannelPair(OHttpVersion version, OHttpCryptoProvider clientProvider,
-                                                OHttpCryptoProvider serverProvider,
+                                                OHttpCryptoProvider serverProvider, KEM kem,
                                                 OHttpClientCodecBuilder clientCodecBuilder,
                                                 OHttpServerCodecBuilder serverCodecBuilder) throws Exception {
-        AsymmetricCipherKeyPair kpR = OHttpCryptoTest.createX25519KeyPair(serverProvider,
-                "3c168975674b2fa8e465970b79c8dcf09f1c741626480bd4c6162fc5b6a98e1a");
+        Assumptions.assumeTrue(clientProvider.isSupported(kem));
+        Assumptions.assumeTrue(serverProvider.isSupported(kem));
+        AsymmetricCipherKeyPair kpR = serverProvider.newRandomPrivateKey(kem);
         byte keyId = 0x66;
 
         OHttpServerKeys serverKeys = new OHttpServerKeys(
                 OHttpKey.newPrivateKey(
                         keyId,
-                        KEM.X25519_SHA256,
+                        kem,
                         Arrays.asList(
                                 OHttpKey.newCipher(KDF.HKDF_SHA256, AEAD.AES_GCM128),
                                 OHttpKey.newCipher(KDF.HKDF_SHA256, AEAD.CHACHA20_POLY1305)),
                         kpR));
 
         OHttpCiphersuite ciphersuite = new OHttpCiphersuite(keyId,
-                KEM.X25519_SHA256,
+                kem,
                 KDF.HKDF_SHA256,
                 AEAD.AES_GCM128);
 
         AsymmetricKeyParameter publicKey = clientProvider.deserializePublicKey(
-                KEM.X25519_SHA256, kpR.publicParameters().encoded());
+                kem, kpR.publicParameters().encoded());
 
         final OHttpClientCodecBuilder clientBuilder = clientCodecBuilder.clone()
                 .setProvider(clientProvider)
@@ -276,9 +289,9 @@ public class OHttpCodecsTest {
     @ParameterizedTest
     @ArgumentsSource(value = OHttpVersionArgumentsProvider.class)
     void testContent(OHttpVersion version, OHttpCryptoProvider clientProvider,
-                     OHttpCryptoProvider serverProvider, boolean useTrailers)
+                     OHttpCryptoProvider serverProvider, KEM kem, boolean useTrailers)
             throws Exception {
-        ChannelPair channels = createChannelPair(version, clientProvider, serverProvider);
+        ChannelPair channels = createChannelPair(version, clientProvider, serverProvider, kem);
         EmbeddedChannel client = channels.client();
         EmbeddedChannel server = channels.server();
 
@@ -324,12 +337,12 @@ public class OHttpCodecsTest {
     @ParameterizedTest
     @ArgumentsSource(value = OHttpVersionArgumentsProvider.class)
     void testContentChunked(OHttpVersion version, OHttpCryptoProvider clientProvider,
-                            OHttpCryptoProvider serverProvider, boolean useTrailers)
+                            OHttpCryptoProvider serverProvider, KEM kem, boolean useTrailers)
             throws Exception {
 
         assumeTrue(version != OHttpVersionDraft.INSTANCE);
 
-        ChannelPair channels = createChannelPair(version, clientProvider, serverProvider);
+        ChannelPair channels = createChannelPair(version, clientProvider, serverProvider, kem);
         EmbeddedChannel client = channels.client();
         EmbeddedChannel server = channels.server();
         HttpHeaders trailers = newTrailers(useTrailers);
@@ -383,9 +396,9 @@ public class OHttpCodecsTest {
     @ParameterizedTest
     @ArgumentsSource(value = OHttpVersionArgumentsProvider.class)
     void testCodec(OHttpVersion version, OHttpCryptoProvider clientProvider,
-                   OHttpCryptoProvider serverProvider, boolean useTrailers) throws Exception {
+                   OHttpCryptoProvider serverProvider, KEM kem, boolean useTrailers) throws Exception {
 
-        ChannelPair channels = createChannelPair(version, clientProvider, serverProvider);
+        ChannelPair channels = createChannelPair(version, clientProvider, serverProvider, kem);
         EmbeddedChannel client = channels.client();
         EmbeddedChannel server = channels.server();
 
@@ -419,13 +432,13 @@ public class OHttpCodecsTest {
     @ParameterizedTest
     @ArgumentsSource(value = OHttpVersionArgumentsProvider.class)
     void testServerMaxFieldSection(OHttpVersion version, OHttpCryptoProvider clientProvider,
-                   OHttpCryptoProvider serverProvider, boolean useTrailers) throws Exception {
+                   OHttpCryptoProvider serverProvider, KEM kem, boolean useTrailers) throws Exception {
         OHttpServerCodecBuilder serverCodecConfig = new OHttpServerCodecBuilder();
         // Make the max field section size very small, so we make cause it to trigger
         // and give us a BAD_REQUEST return code.
         serverCodecConfig.setMaxFieldSectionSize(64);
 
-        ChannelPair channels = createChannelPair(version, clientProvider, serverProvider,
+        ChannelPair channels = createChannelPair(version, clientProvider, serverProvider, kem,
                 new OHttpClientCodecBuilder(), serverCodecConfig);
         EmbeddedChannel client = channels.client();
         EmbeddedChannel server = channels.server();